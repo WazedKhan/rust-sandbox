179f4c25a1ccbeba7cecaafd07a305794b121c31
Wajed Khan <wajed@hjemla.no>
2026-08-11 22:03:25 +0600

    feat: loops handling

---
+++ loops/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let mut count:u32 = 0;
+    let result = loop {
+        count += 1;
+        if count == 10 {
+            break count * 2;
+        }
+    };
+    println!("The result is: {result}");
+}
